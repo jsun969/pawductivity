bcc7cfee8fff319c7bedda4bfbc5d91ac5f7c03d
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2025-03-15 16:54:29 +1030

    feat: Add idle animation for pet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,20 @@
 import { Stack } from 'expo-router';
+import { useRef, useEffect } from 'react';
 import { View, Text, Image } from 'react-native';
+import { Sprites, type SpritesMethods } from 'react-native-sprites';
 
 export default function Home() {
+  const spriteRef = useRef<SpritesMethods>(null);
+
+  useEffect(() => {
+    spriteRef.current?.play({
+      type: 'idle',
+      fps: 5,
+      loop: true,
+      resetAfterFinish: true,
+    });
+  }, []);
+
   return (
     <>
       <Stack.Screen options={{ title: 'Pet' }} />
@@ -14,6 +27,17 @@ export default function Home() {
             />
             <Text className="text-lg font-bold">100</Text>
           </View>
+        </View>
+        <View className="scale-[10] self-center">
+          <Sprites
+            ref={spriteRef}
+            source={require('../../assets/spritesheets/Cat-1-Idle.png')}
+            columns={10}
+            rows={1}
+            animations={{
+              idle: { row: 0, startFrame: 0, endFrame: 9 },
+            }}
+          />
         </View>
       </View>
     </>
