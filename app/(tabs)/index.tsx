e1430bd628dd793470d59e0d6f96cc5a7430c698
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2025-03-15 12:32:41 +1030

    fix: Make coin image size normal on web

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,10 @@ export default function Home() {
       <View className="flex-1 p-6">
         <View className="mb-4 flex-row items-center justify-between">
           <View className="flex-row items-center">
-            <Image source={require('../../assets/images/coin.png')} className="mr-2 h-6 w-6" />
+            <Image
+              source={require('../../assets/images/coin.png')}
+              style={{ marginRight: 8, height: 24, width: 24 }}
+            />
             <Text className="text-lg font-bold">100</Text>
           </View>
         </View>
