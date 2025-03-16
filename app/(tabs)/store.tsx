bfef7f0627d9a6d90104565e60f0a6d07caf5214
shxhid <shahid2677@hotmail.com>
2025-03-16 14:51:15 +1030

    remove debug cois in store page

--- app/(tabs)/store.tsx
+++ app/(tabs)/store.tsx
@@ -159,21 +159,6 @@ export default function ExpandableList() {
 
   return (
     <ScrollView className="flex-1 p-4">
-      <View className="flex-row items-center">
-        <Canvas style={{ width: 35, height: 35 }}>
-          <SkiaImage
-            image={useImage(require('../../assets/images/coin.png'))}
-            x={0}
-            y={0}
-            width={35}
-            height={35}
-          />
-        </Canvas>
-        <Text className="text-4xl font-bold">{coins}</Text>
-      </View>
-      <Button title="add 100 coins" onPress={() => setCoins((prev) => prev + 100)}>
-        more coins
-      </Button>
       {STORE.map(({ category, items }, index) => {
         const isCategoryOpen = openedCategories.has(category);
         return (
